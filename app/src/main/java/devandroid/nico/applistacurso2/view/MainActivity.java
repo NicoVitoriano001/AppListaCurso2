b1ae2251418d79b21d70204f0f223c686011075f
NicoVitoriano001 <nicovitoriano@gmail.com>
2023-12-24 18:39:25 -0300

    35. 2023 - Como programar o EditText e Button do Layout na Classe MainActivity.java

--- app/src/main/java/devandroid/nico/applistacurso2/view/MainActivity.java
+++ app/src/main/java/devandroid/nico/applistacurso2/view/MainActivity.java
@@ -1,22 +1,24 @@
 package devandroid.nico.applistacurso2.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import devandroid.nico.applistacurso2.R;
-
 import devandroid.nico.applistacurso2.model.Pessoa;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa; //marcou objeto pessoa para Classe Pessoa(foi com mesmo nome, mas pode ser diferente)
     Pessoa outraPessoa;
     String dadosPessoa;
-
     String dadosOutraPessoa;
 
     EditText editPrimeiroNome;
@@ -33,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa(); //instanciou o objeto pessoa na Classe Pessoa
-        pessoa.setPrimeiroNome("Nico"); //variável pessoa para chamar o método setPrimeiroNome(). atribuir dados/valores para o objeto pessoa
+      /*  pessoa.setPrimeiroNome("Nico"); //variável pessoa para chamar o método setPrimeiroNome(). atribuir dados/valores para o objeto pessoa
         pessoa.setSobreNome("Vit");
         pessoa.setCursoDesejado("Android");
-        pessoa.setTelefoneContato("11 8383838383");
+        pessoa.setTelefoneContato("11 8383838383");*/
         //ver menu lateral Structure
 
         outraPessoa = new Pessoa();
@@ -50,14 +52,51 @@ public class MainActivity extends AppCompatActivity {
         editNomeCurso = findViewById(R.id.editNomeCurso);
         editTelefoneContato = findViewById(R.id.editTelefoneContato);
 
+
         btnLimpar = findViewById(R.id.btnLimpar);
         btnSalvar = findViewById(R.id.btnSalvar);
         btnFinalizar = findViewById(R.id.btnFinalizar);
+
 
         editPrimeiroNome.setText(pessoa.getPrimeiroNome());
         editSobrenomeAluno.setText(pessoa.getSobreNome());
         editNomeCurso.setText(pessoa.getCursoDesejado());
         editTelefoneContato.setText(pessoa.getTelefoneContato());
+
+
+        btnLimpar.setOnClickListener(new View.OnClickListener() { //teve que criar/new uma instancia dentro do onclicklistener
+            @Override  //tostring lembra???
+            public void onClick(View v) {
+                editPrimeiroNome.setText("rr");
+                editSobrenomeAluno.setText("");
+                editNomeCurso.setText("");
+                editTelefoneContato.setText("");
+            }
+        });
+
+        btnFinalizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+                Toast.makeText(MainActivity.this, "Volte Sempre", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                pessoa.setPrimeiroNome(editPrimeiroNome.getText().toString());
+                pessoa.setSobreNome(editSobrenomeAluno.getText().toString());
+                pessoa.setCursoDesejado(editNomeCurso.getText().toString());
+                pessoa.setTelefoneContato(editTelefoneContato.getText().toString());
+
+                Toast.makeText(MainActivity.this, "Salvo "+pessoa.toString(), Toast.LENGTH_LONG).show();
+
+            }
+        });
 
 /*      dadosPessoa = "Primeiro nome: ";
         dadosPessoa += pessoa.getPrimeiroNome();
@@ -80,6 +119,17 @@ public class MainActivity extends AppCompatActivity {
 //feito para debugar no Logcat
         Log.i("POOAndroid", pessoa.toString()); //mesma função das 8 linhas acima
         Log.i("POOAndroid", outraPessoa.toString());
+
+
+       // Obtém a data e hora atuais
+       //     Date dataHoraAtual = new Date();
+    // Formata a data e hora atuais
+      //      SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+       //     String dataHoraFormatada = sdf.format(dataHoraAtual);
+    // Exibe a data e hora atuais no toast
+       //     Toast.makeText(this, dataHoraFormatada, Toast.LENGTH_SHORT).show();
+
+
 
     }
 }
